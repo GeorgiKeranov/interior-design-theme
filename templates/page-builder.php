b046aaa0d5332a0d0efd5965629abaaf0d9475c2
Georgi <georgy.keranov@gmail.com>
2021-07-25 22:34:19 +0300

    Remove element from array in page builder sections

--- templates/page-builder.php
+++ templates/page-builder.php
@@ -5,7 +5,6 @@
 
 the_post();
 
-
 get_header();
 
 $sections = carbon_get_the_post_meta( 'idt_page_builder_sections' );
@@ -13,7 +12,13 @@ $sections = carbon_get_the_post_meta( 'idt_page_builder_sections' );
 foreach ( $sections as $section ) {
 	$template_path = 'template-parts/page-builder/' . $section['_type'];
 
-	get_template_part($template_path, null, $section );
+	/**
+	 * Remove '_type' element from the array so in the section template we
+	 * can easily detect if all of the fields are empty
+	 */
+	unset( $section['_type'] );
+
+	get_template_part( $template_path, null, $section );
 }
 
 get_footer();

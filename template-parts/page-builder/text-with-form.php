0b6a56d5190fee0fa3a3c12bac3ff30a264f3c27
Georgi <georgy.keranov@gmail.com>
2021-08-06 14:20:10 +0300

    Change inner section to be global for the whole site

--- template-parts/page-builder/text-with-form.php
+++ template-parts/page-builder/text-with-form.php
@@ -1,8 +1,9 @@
 <div class="section-txt-with-form">
-	<div class="section__form">
-		<div class="section__content">
+	<div class="section-form">
+		<div class="section__text">
 			<?php echo apply_filters( 'the_content', $args['text'] ) ?>
-		</div><!-- /.section__content -->
+		</div><!-- /.section__text -->
+		
 		<?php echo do_shortcode( $args['form_shortcode'] ) ?>
-	</div><!-- /.section__form -->
+	</div><!-- /.section-form -->
 </div><!-- /.section-txt-with-form -->

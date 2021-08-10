e41c0d527b3d3b62f6b67df47f7774323c8a790d
Georgi <georgy.keranov@gmail.com>
2021-08-10 14:05:58 +0300

    Exclude sections without fields from empty sections

--- templates/page-builder.php
+++ templates/page-builder.php
@@ -9,14 +9,20 @@ get_header();
 
 $sections = carbon_get_the_post_meta( 'idt_page_builder_sections' );
 
+$sections_without_fields = array(
+	'projects',
+);
+
 foreach ( $sections as $section ) {
-	$template_path = 'template-parts/page-builder/' . $section['_type'];
+	$section_type = $section['_type'];
+
+	$template_path = 'template-parts/page-builder/' . $section_type;
 
 	/**
 	 * Remove '_type' element from the array so in the section template we
 	 * can easily detect if all of the fields are empty
 	 */
-	unset( $section['_type'] );
+	unset( $section_type );
 
 	/**
 	 * Remove empty fields from array so we can check if there are any filled fields
@@ -25,8 +31,10 @@ foreach ( $sections as $section ) {
 
 	/**
 	 * Do not import the section in the page if all of the fields for it are empty
+	 *
+	 * Exceptions to this rule are the sections that are declared without fields
 	 */
-	if ( empty( $section ) ) {
+	if ( empty( $section ) && !in_array( $section_type, $sections_without_fields ) ) {
 		continue;
 	}
 

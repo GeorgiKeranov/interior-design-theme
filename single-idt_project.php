8bac5886eb358d487f642cb54a2d1e722537c788
Georgi <georgy.keranov@gmail.com>
2021-08-10 21:32:56 +0300

    Add folder for single-idt_project sections

--- single-idt_project.php
+++ single-idt_project.php
@@ -4,10 +4,10 @@ the_post();
 
 get_header();
 
-get_template_part('template-parts/section-content');
+get_template_part('template-parts/single-idt_project/section-content');
 
-get_template_part('template-parts/section-gallery');
+get_template_part('template-parts/single-idt_project/section-gallery');
 
-get_template_part('template-parts/section-related-projects');
+get_template_part('template-parts/single-idt_project/section-related-projects');
 
 get_footer();

c1ee86a5b450cdfc3b0b96e0bd052a6be325dae3
Georgi <georgy.keranov@gmail.com>
2021-08-11 11:05:34 +0300

    Add handler template parts for single and archive pages

--- page.php
+++ page.php
@@ -13,20 +13,7 @@
  */
 
 get_header();
-?>
 
-	<main id="primary" class="site-main">
+get_template_part( 'template-parts/single', 'content' );
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
 get_footer();

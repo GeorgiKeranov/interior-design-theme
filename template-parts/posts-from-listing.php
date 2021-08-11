094c84bfb3379204ebba85a27c0fcf6a813ccf20
Georgi <georgy.keranov@gmail.com>
2021-08-11 16:09:33 +0300

    Add responsive styles for section posts

--- template-parts/posts-from-listing.php
+++ template-parts/posts-from-listing.php
@@ -6,21 +6,23 @@ if ( empty( $posts ) ) {
 foreach ( $posts as $post ) :
 	$post_id = $post->ID;
 	$post_title = $post->post_title;
-	$post_thumbnail = get_the_post_thumbnail_url( $post_id, 'medium_large_fixed' );
+	$post_thumbnail_url = get_the_post_thumbnail_url( $post_id, 'medium_large_fixed' );
 	$post_permalink = get_the_permalink( $post_id ); ?>
 	
 	<div class="section__post">
 		<div class="section__image">
-			<div class="section__image-background" style="background-image: url(<?php echo $post_thumbnail ?>)">
-			</div><!-- /.section__image-background -->
-
-			<div class="section__title">
-				<h2><a href="<?php echo $post_permalink ?>"><?php echo esc_html( $post_title ) ?></a></h2>
-			</div><!-- /.section__title -->
-			
-			<div class="section__view-post">
-				<a href="<?php echo $post_permalink ?>"><?php _e( 'НАУЧЕТЕ ПОВЕЧЕ', 'idt' ) ?> <?php echo idt_get_svg('icon-arrow-right') ?></a>
-			</div><!-- /.section__view-post -->
+			<a href="<?php echo $post_permalink ?>">
+				<div class="section__image-background" style="background-image: url(<?php echo $post_thumbnail_url ?>)">
+				</div><!-- /.section__image-background -->
+			</a>
 		</div><!-- /.section__image -->
+
+		<div class="section__title">
+			<h2><a href="<?php echo $post_permalink ?>"><?php echo esc_html( $post_title ) ?></a></h2>
+		</div><!-- /.section__title -->
+		
+		<div class="section__view-post">
+			<a href="<?php echo $post_permalink ?>"><?php _e( 'НАУЧЕТЕ ПОВЕЧЕ', 'idt' ) ?> <?php echo idt_get_svg('icon-arrow-right') ?></a>
+		</div><!-- /.section__view-post -->
 	</div><!-- /.section__post -->
 <?php endforeach; ?>

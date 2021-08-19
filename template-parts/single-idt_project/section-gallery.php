db85bd03cc4fc3f5db1dfbff29cc3d3373b67610
Georgi <georgy.keranov@gmail.com>
2021-08-19 23:58:37 +0300

    Change section gallery size and image size

--- template-parts/single-idt_project/section-gallery.php
+++ template-parts/single-idt_project/section-gallery.php
@@ -9,19 +9,21 @@ get_template_part('template-parts/pswp-static-html');
 ?>
 
 <div class="section-gallery">
-	<div class="section__columns">
-		<?php foreach ( $gallery as $image ) :
-			$image_full_size_src = wp_get_attachment_image_src( $image, 'full' );
-			$image_full_size_url = $image_full_size_src[0];
-			$image_full_size_width_and_height = $image_full_size_src[1] . 'x' . $image_full_size_src[2];
+	<div class="container">
+		<div class="section__columns">
+			<?php foreach ( $gallery as $image ) :
+				$image_full_size_src = wp_get_attachment_image_src( $image, 'full' );
+				$image_full_size_url = $image_full_size_src[0];
+				$image_full_size_width_and_height = $image_full_size_src[1] . 'x' . $image_full_size_src[2];
 
-			$image_small = wp_get_attachment_image( $image, 'medium_fixed' );
-			?>
-			<figure>
-				<a href="<?php echo $image_full_size_url ?>" data-size="<?php echo $image_full_size_width_and_height ?>">
-					<?php echo $image_small ?>
-				</a>
-			</figure>
-		<?php endforeach; ?>
-	</div><!-- /.section__columns -->
+				$image_small = wp_get_attachment_image( $image, 'large' ); ?>
+				
+				<figure>
+					<a href="<?php echo $image_full_size_url ?>" data-size="<?php echo $image_full_size_width_and_height ?>">
+						<?php echo $image_small ?>
+					</a>
+				</figure>
+			<?php endforeach; ?>
+		</div><!-- /.section__columns -->
+	</div><!-- /.container -->
 </div><!-- /.section-gallery -->

b6aa3f7b9bea6c1b5bb7e053f45d986114ca3a69
Georgi <georgy.keranov@gmail.com>
2021-10-05 19:04:08 +0300

    Add if statement before initializing variables

--- inc/button.php
+++ inc/button.php
@@ -1,14 +1,14 @@
 <?php
 
 function idt_render_button( $label, $link, $new_tab, $class = 'btn' ) {
+	if ( empty( $label ) || empty( $link ) ) {
+		return;
+	}
+
 	$label = esc_html( $label );
 	$link = esc_url( $link );
 	$target = '';
 	$class = ' class="' . $class . '"';
-
-	if ( empty( $label ) || empty( $link ) ) {
-		return;
-	}
 
 	if ( !empty( $new_tab ) && $new_tab === 'yes' ) {
 		$target = ' target="_blank"';

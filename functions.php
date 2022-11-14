93a72cd274ca7d6e05683ca8a5bf6de98e8e63d5
Georgi Keranov <georgy.keranov@gmail.com>
2022-11-14 22:06:54 +0200

    Stop email notifications for automatic updates

--- functions.php
+++ functions.php
@@ -250,8 +250,30 @@ require IDT_THEME_DIR . '/inc/page-functions.php';
  */
 require IDT_THEME_DIR . '/inc/admin-pages-customization.php';
 
-
 /**
  * Add functionalities for dynamic logo on header and footer
  */
 require IDT_THEME_DIR . '/inc/logo-dynamic.php';
+
+/**
+ * Stop the email notifications for automatic plugin updates
+ */
+add_filter( 'auto_plugin_update_send_email', '__return_false' );
+
+
+/**
+ * Stop the email notifications for automatic theme updates
+ */
+add_filter( 'auto_theme_update_send_email', '__return_false' );
+
+/**
+ * Stop the email notifications for automatic core updates
+ */
+add_filter( 'auto_core_update_send_email', 'wpb_stop_auto_update_emails', 10, 4 );
+function wpb_stop_update_emails( $send, $type, $core_update, $result ) {
+	if ( ! empty( $type ) && $type == 'success' ) {
+		return false;
+	}
+	
+	return true;
+}

144c4fb55e32c156672aa7a3df540b32ba87aa47
Georgi Keranov <georgy.keranov@gmail.com>
2022-12-11 20:31:34 +0200

    Fix complex over complex bug in admin panel

--- inc/admin-pages-customization.php
+++ inc/admin-pages-customization.php
@@ -5,3 +5,7 @@ function idt_add_custom_login_styles() {
 	echo '<link rel="stylesheet" type="text/css" href="'. get_bloginfo( 'stylesheet_directory' ) . '/admin-login.css" />';
 }
 
+add_action('admin_head', 'idt_add_custom_admin_styles');
+function idt_add_custom_admin_styles() {
+	echo '<link rel="stylesheet" type="text/css" href="'. get_bloginfo( 'stylesheet_directory' ) . '/admin.css" />';
+}

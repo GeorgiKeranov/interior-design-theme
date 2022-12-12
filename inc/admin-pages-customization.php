fac086241c9f95a827e6ca1a9a8c4b0ec704314c
Georgi Keranov <georgy.keranov@gmail.com>
2022-12-13 01:00:08 +0200

    Remove admin panel styles

--- inc/admin-pages-customization.php
+++ inc/admin-pages-customization.php
@@ -4,8 +4,3 @@ add_action( 'login_head', 'idt_add_custom_login_styles' );
 function idt_add_custom_login_styles() {
 	echo '<link rel="stylesheet" type="text/css" href="'. get_bloginfo( 'stylesheet_directory' ) . '/admin-login.css" />';
 }
-
-add_action('admin_head', 'idt_add_custom_admin_styles');
-function idt_add_custom_admin_styles() {
-	echo '<link rel="stylesheet" type="text/css" href="'. get_bloginfo( 'stylesheet_directory' ) . '/admin.css" />';
-}
